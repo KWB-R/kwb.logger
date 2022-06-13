3aaf3171e97f09b9e264596a3287a82f5fd22afe
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-06-13 11:29:18 +0200

    Add extdataFile(), ceprecate exampleLoggerFiles()

--- R/logger_General.R
+++ R/logger_General.R
@@ -3,13 +3,28 @@
 #' Example Logger Files
 #' 
 #' @return full path(s) to example file(s)
-#' 
 #' @export
 #' 
-exampleLoggerFiles <- function() 
+exampleLoggerFiles <- function()
 {
-  dir(
-    path = system.file("extdata", package = "kwb.logger"), recursive = TRUE, 
-    full.names = TRUE
-  )
+  kwb.utils::warningDeprecated("exampleLoggerFiles", "extdataFile")
+  dir(extdataFile(), recursive = TRUE, full.names = TRUE)
 }
+
+# For a template of the following Roxygen documentation, see:
+# writeLines(readLines("../kwb.utils/R/createFunctionExtdataFile.R"))
+
+#' Path to File in Installed Package
+#' 
+#' @param \dots parts of the file path to be passed to \code{\link{system.file}}
+#' @param must_exist if \code{TRUE} (the default) and the specified file does 
+#'   not exist, the program stops with an error message
+#' @return path to file in the package installation folder in the R library
+#'   or "" if the path does not exist
+#' @return path to the specified file
+#' @importFrom kwb.utils createFunctionExtdataFile
+#' @export
+#' @examples 
+#' # List the files provided in the "extdata" folder of kwb.logger
+#' dir(extdataFile())
+extdataFile <- kwb.utils::createFunctionExtdataFile("kwb.logger")
